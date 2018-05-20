1d48fc78abe06a6ef9892710879312c3d83dbf72
alexkandres <alexkandres@live.com>
2018-05-19 21:42:31 -0400

    append method finish

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -1,5 +1,32 @@
 package com.company;
 
 public class LinkedList {
+    Node head;
 
+    //todo append to end
+    public void append(int data){
+
+        //check special case
+        if(head == null){
+            head = new Node(data);
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null){
+            current = current.next;
+        }
+        current.next = new Node(data);
+
+    }
+    //todo prepend to beginning
+    //todo delete
+    //todo print linkedlist
+    public void printList(){
+        Node current = head;
+        while (current != null){
+            System.out.print(current.data + ", ");
+            current = current.next;
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //1. create LinkedList
+        LinkedList linkedList = new LinkedList();
+        //2. linkedLst.append data(multiple data)
+        linkedList.append(45);
+        linkedList.append(55);
+        linkedList.append(7);
+        //3. printout linkelist
+        linkedList.printList();
+        //4. deletewithvalue
     }
 }
